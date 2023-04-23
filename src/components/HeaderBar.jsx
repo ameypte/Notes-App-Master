473416de0cd4eae30359f2b3937bf4f8c677d240
Anant Deshmukh <anant7564@gmail.com>
2023-04-23 11:53:39 +0530

    final commmit

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,42 +1,52 @@
-
 import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
-import { Menu } from '@mui/icons-material';
+import { Menu, Logout } from '@mui/icons-material';
 import { styled } from '@mui/material/styles';
-import logo from "./imeges/GPA_logo.png";
+import logo from "./images/GPA_logo.png";
+import { useNavigate } from 'react-router-dom';
 
 const Header = styled(AppBar)`
   z-index: 1201;
   background: #fff;
   height: 70px;
   box-shadow: inset 0 -1px 0 0 #dadce0;
-`
+`;
 
 const Heading = styled(Typography)`
   color: #5F6368;
   font-size: 24px;
   margin-left: 25px;
-  
-`
+`;
 
+const LogoutButton = styled(IconButton)`
+  margin-left: auto;
+`;
 
 const HeaderBar = ({ open, handleDrawer }) => {
-  // const logo = 'https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png';
+  const navigate = useNavigate();
   
+  const handleLogout = () => {
+    localStorage.setItem("user","");
+    navigate("/")
+  };
+
   return (
     <Header open={open}>
       <Toolbar>
         <IconButton
           onClick={() => handleDrawer()}
-          sx={{ marginRight: '20px'}}
+          sx={{ marginRight: '20px' }}
           edge="start"
         >
           <Menu />
         </IconButton>
-        <img src={logo} alt="logo" style={{width: 30}} />
+        <img src={logo} alt="logo" style={{ width: 30 }} />
         <Heading>Notes App</Heading>
+        <LogoutButton onClick={handleLogout} edge="end">
+          <Logout />
+        </LogoutButton>
       </Toolbar>
     </Header>
   )
-}
+};
 
 export default HeaderBar;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,17 +7,22 @@ import Notes from "./notes/Notes";
 import Archives from "./archives/Archives";
 import DeleteNotes from "./delete/DeleteNotes";
 import UploadFiles from "./upload/UploadFiles";
+import Login from "./login/Login";
+import Register from "./login/Register"
 
 const Home = () => {
   return (
     <Box style={{ display: "flex", width: "100%" }}>
       <Router>
-        <SwipeDrawer />
+
         <Routes>
-          <Route path="/" element={<Notes />} />
-          <Route path="/archive" element={<Archives />} />
-          <Route path="/delete" element={<DeleteNotes />} />
-          <Route path="/upload" element={<UploadFiles />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
+
+          <Route path="/notes" element={<><SwipeDrawer /> <Notes /></>} />
+          <Route path="/archive" element={<><SwipeDrawer /> <Archives /></>} />
+          <Route path="/delete" element={<><SwipeDrawer /> <DeleteNotes /></>} />
+          <Route path="/upload" element={<><SwipeDrawer /> <UploadFiles /></>} />
         </Routes>
       </Router>
     </Box>
